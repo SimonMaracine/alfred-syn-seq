cf971fb41a0c96aa38a0ce298f3c7f06546ceec3
SimonMaracine <simonmara.dev@gmail.com>
2026-01-10 00:51:50 +0200

    Improve CMake build scripts

---
+++ cmake/sanity_checks.cmake
@@ -0,0 +1,11 @@
+if(NOT UNIX AND NOT WIN32)
+    message(FATAL_ERROR "Operating system is not Linux or Windows")
+endif()
+
+if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    message(FATAL_ERROR "Compiler is not GCC or MSVC")
+endif()
+
+if(ALFRED_DISTRIBUTION AND NOT CMAKE_BUILD_TYPE STREQUAL "Release")
+    message(FATAL_ERROR "Distribution mode requires release mode")
+endif()
---
+++ cmake/target_options.cmake
@@ -0,0 +1,23 @@
+function(set_target_cpp_mode TARGET)
+    target_compile_features(${TARGET} PRIVATE cxx_std_23)
+    set_target_properties(${TARGET} PROPERTIES CXX_EXTENSIONS OFF)
+endfunction()
+
+function(enable_target_warnings TARGET)
+    if(UNIX)
+        target_compile_options(${TARGET} PRIVATE "-Wall" "-Wextra" "-Wpedantic" "-Wconversion")
+    elseif(WIN32)
+        target_compile_options(${TARGET} PRIVATE "/W4")
+    endif()
+endfunction()
+
+function(enable_target_diagnostics TARGET)
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND UNIX)
+        target_compile_options(${TARGET} PRIVATE "-g")
+    endif()
+
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND UNIX AND ALFRED_ASAN)
+        target_compile_options(${TARGET} PRIVATE "-fsanitize=address" "-fsanitize=undefined" "-fno-omit-frame-pointer")
+        target_link_options(${TARGET} PRIVATE "-fsanitize=address" "-fsanitize=undefined")
+    endif()
+endfunction()
