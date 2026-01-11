711d104691cdb6dd76447b1c5632e37eec7e989f
SimonMaracine <simonmara.dev@gmail.com>
2026-01-11 02:14:23 +0200

    Clean up and improve main

--- alfred/gui/src/main.cpp
+++ alfred/gui/src/main.cpp
@@ -5,23 +5,32 @@
 
 #include "application.hpp"
 
+static void set_property(const char* property, const char* value) {
+    (void) SDL_SetAppMetadataProperty(property, value);
+}
+
+static void show_error_message_box(const char* title, const char* message) {
+    (void) SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, message, nullptr);
+}
+
 int main(int argc, char** argv) {
-    (void) SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, "Alfred");
-    (void) SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_VERSION_STRING, "0.1.0");
-    (void) SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_IDENTIFIER_STRING, "dev.simonmara.alfred");
-    (void) SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_CREATOR_STRING, "Simon");
-    (void) SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_URL_STRING, "https://github.com/SimonMaracine");
-    (void) SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_TYPE_STRING, "application");
+    set_property(SDL_PROP_APP_METADATA_NAME_STRING, "Alfred");
+    set_property(SDL_PROP_APP_METADATA_VERSION_STRING, "0.1.0");
+    set_property(SDL_PROP_APP_METADATA_IDENTIFIER_STRING, "dev.simonmara.alfred");
+    set_property(SDL_PROP_APP_METADATA_CREATOR_STRING, "Simon");
+    set_property(SDL_PROP_APP_METADATA_URL_STRING, "https://github.com/SimonMaracine");
+    set_property(SDL_PROP_APP_METADATA_TYPE_STRING, "application");
 
     try {
         Application application;
         application.run();
     } catch (const VideoError& e) {
         std::cerr << "Fatal video error: " << e.what() << '\n';
+        show_error_message_box("Alfred Video Error", "A critical video error occurred. Check the logs.");
         return 1;
     } catch (const audio::AudioError& e) {
         std::cerr << "Fatal audio error: " << e.what() << '\n';
-        (void) SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Alfred Audio Error", "A critical audio error occurred. Check the log.", nullptr);
+        show_error_message_box("Alfred Audio Error", "A critical audio error occurred. Check the logs.");
         return 1;
     }
 
