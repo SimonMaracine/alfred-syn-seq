bf44391ad1cdba8f1fd04f5c2ed5a12fb4753fe7
SimonMaracine <simonmara.dev@gmail.com>
2026-03-05 10:21:25 +0200

    Make some improvements to the packaging code

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -6,21 +6,23 @@ elseif(WIN32)
     set(CPACK_SOURCE_GENERATOR "ZIP")
 endif()
 
-#if(WIN32)
-#    set(CPACK_PACKAGE_INSTALL_DIRECTORY "Alfred")
-#endif()
+if(UNIX)
+    # The default, which we don't want, was /usr
+    set(CPACK_PACKAGING_INSTALL_PREFIX "/usr/local")
+endif()
 
 set(CPACK_SOURCE_IGNORE_FILES "build\/.*;\.env\/.*;\.git\/.*;analysis\/.*;\.vscode\/.*;\.idea\/.*;extern\/.*")
 set(CPACK_PACKAGE_VENDOR "Simon")
 set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/app/res/icon128.png")
 set(CPACK_PACKAGE_DESCRIPTION "Alfred is a free and open source cross-platform synthesizer and sequencer program, which allows you to synthesize different instruments and to compose and play music.")
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A cross-platform synthesizer and sequencer program")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${CMAKE_PROJECT_DESCRIPTION}")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE")
 set(CPACK_PACKAGE_EXECUTABLES "alfred;Alfred")
+set(CPACK_MONOLITHIC_INSTALL TRUE)
 set(CPACK_VERBATIM_VARIABLES TRUE)
+
+set(CPACK_RPM_PACKAGE_REQUIRES "fftw")
 
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "simonmara.dev@gmail.com")
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libfftw3-double3")
-
-set(CPACK_RPM_PACKAGE_REQUIRES "fftw")
